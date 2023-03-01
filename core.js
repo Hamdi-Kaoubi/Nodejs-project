56bb8598091ff759aa2bc5383afb7b07034c20fa
Hamdi-Kaoubi <hamdi.kaoubi1994@gmail.com>
2023-03-01 09:55:23 +0100

    nodejs

---
+++ core.js
@@ -0,0 +1,13 @@
+const fs=require("fs")
+const http=require("http")
+fs.readFile("./f.txt","utf8",function(err,data){
+    if(err){
+        console.log(err)
+    }
+    else {
+        console.log(data)
+    }
+})
+console.log("starting")
+console.log("finishing")
+console.log(http)
---
+++ local.js
@@ -0,0 +1,4 @@
+module.exports={
+    min:(a,b)=>a-b,
+    add:(a,b)=>a+b
+}
